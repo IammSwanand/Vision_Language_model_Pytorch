cb1cbe2dbaf5315a769cff242fa94de73379c571
IammSwanand <swanandwakadmane94@gmail.com>
2025-11-12 17:17:26 +0530

    added Attention Layer in Encoder

--- SigLip_Modeling.py
+++ SigLip_Modeling.py
@@ -69,6 +69,34 @@ class SiglipVisionEmbeddings(nn.Module):
         #add comment here
         return embeddings
     
+class SiglipAttention(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.embed_dim = config.hidden_size
+        self.num_heads = config.num_attention_heads
+        self.head_dim = self.embed_dim // self.num_heads
+        self.scale = self.head_dim**-0.5
+        self.dropout = config.attention_dropout
+
+        self.k_proj = nn.Linear(self.embed_dim, self.embed_dim)
+        self.v_proj = nn.Linear(self.embed_dim, self.embed_dim)
+        self.q_proj = nn.Linear(self.embed_dim, self.embed_dim)
+        self.out_proj = nn.Linear(self.embed_dim, self.embed_dim)
+    
+    def forward(
+        self, 
+        hidden_states: torch.Tensor,
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor]]:
+        batch_size, seq_len, _ = hidden_states.size()
+        query_states = self.q_proj(hidden_states)
+        key_states = self.k_proj(hidden_states)
+        value_states = self.v_proj(hidden_states)
+        query_states = query_states.view(batch_size, seq_len, self.num_heads, self.head_dim).transpose(1, 2)
+        key_states = key_states.view(batch_size, seq_len, self.num_heads, self.head_dim).transpose(1, 2)
+        value_states = value_states.view(batch_size, seq_len, self.num_heads, self.head_dim).transpose(1, 2)
+    
 class SiglipMLP(nn.Module):
     def __init__(self, config):
         super().__init__()
@@ -81,7 +109,7 @@ class SiglipMLP(nn.Module):
         hidden_states = nn.functional.gelu(hidden_states, approximate= "tanh")
         hidden_states = self.fc2(hidden_states)
         return hidden_states
-
+    
 class SiglipEncoderLayer(nn.Module):
     def __init__(self, config: SiglipVisionConfig):
         super().__init__()
