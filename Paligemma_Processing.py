fe3e361332e1eb20ce737d81656fbe40ec31e48b
IammSwanand <swanandwakadmane94@gmail.com>
2025-11-26 20:53:00 +0530

    added the image processing funstion and added the Paligemma Processor calling functionality

--- Paligemma_Processing.py
+++ Paligemma_Processing.py
@@ -1,11 +1,36 @@
-from typing import Dict, list, Tuple, Optional, Union, Iterable
+from typing import Dict, List, Tuple, Optional, Union, Iterable
 import numpy as np
 from PIL import Image
 import torch
 IMAGENET_STANDARD_MEAN = [0.5, 0.5, 0.5]
 IMAGENET_STANDARD_STD = [0.5, 0.5, 0.5]
 
+def process_images(
+    images: List[Image.Image],
+    size: Dict[str, int] = None,
+    resample: Image.Resampling = None,
+    rescale_factor: float = None,
+    image_mean: Optional[Union[float, List[float]]] = None,
+    image_std: Optional[Union[float, List[float]]] = None,
+) -> List[np.ndarray]:
+    height, width = size[0], size[1]
+    images = [
+        resize(image=image, size = (height, width), resample=resample) for image in images
+    ]
+
+    images = [np.array(image) for image in images]
+
+    images = [rescale(image, scale=rescale_factor) for image in images]
+
+    images = [normalize(image, mean=image_mean, std=image_std) for image in images]
+
+    images = [image.transpose(2, 0, 1) for image in images]
+    return images
+
+#Gemma_Tokenizer
 class PaliGemmaProcessor:
+
+    IMAGE_TOKEN = "<image>"
     
     def __init__(self, tokenizer, num_image_token: int, image_size: int):
         super().__init__()
@@ -16,10 +41,10 @@ class PaliGemmaProcessor:
         tokens_to_add = {"additional_special_tokens": [self.IMAGE_TOKEN]}
         tokenizer.add_special_tokens(tokens_to_add)
         EXTRA_TOKENS = [
-            f"<loc{i:04d}>" for i in range(1024)
+            f"<loc{i:04d}>" for i in range(1024) #LOCATION TOKENS
         ]
         EXTRA_TOKENS += [
-            f"<img{i:03d}>" for i in range(128)
+            f"<img{i:03d}>" for i in range(128) #IMAGE TOKENS
         ]
         tokenizer.add_tokens(EXTRA_TOKENS)
         self.image_token_id = tokenizer.convert_tokens_to_ids(self.IMAGE_TOKEN)
@@ -28,3 +53,45 @@ class PaliGemmaProcessor:
         tokenizer.add_eos_token = False
 
         self.tokenizer = tokenizer
+
+    def __call__(
+            self,
+            text: List[str],
+            images: List[Image.Image],
+            padding: str = "longest",
+            truncation: bool = True,
+    ) -> dict:
+        assert len(images) == 1 and len(text) == 1, f"Received {len(images)} images for {len(text)} prompts."
+
+        pixel_values = process_images(
+            images,
+            size= (self.image_size, self.image_size),
+            resample= Image.Resampling.BICUBIC,
+            rescale_factor = 1/ 255.0,
+            image_mean = IMAGENET_STANDARD_MEAN,
+            image_std = IMAGENET_STANDARD_STD,
+        )
+
+        pixel_values = np.stack(pixel_values, axis=0) #converted the pixel values into the array
+        pixel_values = torch.tensor(pixel_values) #converted into tensor
+
+        input_strings = [
+            add_image_tokens_to_prompt(
+                prefix_prompt = prompt,
+                bos_token=self.tokenizer.bos_token,
+                image_seq_len=self.image_seq_length,
+                image_token=self.IMAGE_TOKEN,
+            )
+            for prompt in text
+        ]
+
+        inputs = self.tokenizer(
+            input_strings,
+            return_tensors="pt",
+            padding=padding,
+            truncation=truncation,
+        )
+
+        return_data = {"pixel_values": pixel_values, **inputs}
+
+        return return_data
