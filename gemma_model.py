2f0de21f13a816852d7545695585c2bef40506d7
IammSwanand <swanandwakadmane94@gmail.com>
2025-11-28 18:51:30 +0530

    added input ids for text, image and mask

--- gemma_model.py
+++ gemma_model.py
@@ -88,6 +88,34 @@ class PaliGemmaForConditionalGeneration(nn.Module):
     def tie_weights(self):
         return self.language_model.tie_weights()
     
+    def _merge_input_ids_with_image_features(
+        self, image_features: torch.Tensor, inputs_embeds: torch.Tensor, input_ids: torch.Tensor, attention_mask: torch.Tensor, kv_cache: Optional[KVCache] = None
+    ):
+        _, _, embed_dim = image_features.shape
+        batch_size, sequence_length, _ = input_ids.shape
+        dtype, device = inputs_embeds.dtype, inputs_embeds.device
+        scaled_image_features = image_features / (self.config.hidden_size ** 0.5)
+
+        final_embedding = torch.zeros(batch_size, sequence_length, embed_dim, dtype = inputs_embeds.dtype, device=inputs_embeds.device)
+
+        text_mask = (input_ids != self.config.image_token_index) & (input_ids != self.pad_token_id)
+
+        image_mask = input_ids == self.config.image_token_index
+
+        pad_mask = input_ids == self.pad_token_id
+
+        text_mask_expanded = text_mask.unsqueeze(-1).expand(-1, -1, embed_dim)
+        pad_mask_expanded = pad_mask.unsqueeze(-1).expand(-1, -1, embed_dim)
+        image_mask_expanded = image_mask.unsqueeze(-1).expand(-1, -1, embed_dim)
+
+        final_embedding = torch.where(text_mask_expanded, inputs_embeds, final_embedding)
+
+        final_embedding = final_embedding.masked_scatter(image_mask_expanded, scaled_image_features)
+
+        final_embedding = torch.where(pad_mask_expanded, torch.zeros_like(final_embedding), final_embedding)
+
+
+
     def forward(
             self,
             input_ids: torch.LongTensor = None,
