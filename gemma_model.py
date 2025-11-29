c56f1ac083664bd4b6639d4a3eaf1b6cc419a565
IammSwanand <swanandwakadmane94@gmail.com>
2025-11-30 01:54:50 +0530

    added Gemma Attention Constructor

--- gemma_model.py
+++ gemma_model.py
@@ -102,6 +102,32 @@ class GemmaMLP(nn.Module):
     def forward(self, x):
 
         return self.down_proj(nn.functional.gelu(self.gate_proj(x), approximate = "tanh") * self.up_proj(x))
+    
+
+class GemmaAttention(nn.Module):
+
+    def __init__(self, config: GemmaConfig, layer_idx: Optional[int] = None):
+        super().__init__()
+        self.config = config
+        self.layer_idx = layer_idx
+
+        self.attention_dropout = config.attention_dropout
+        self.hidden_size = config.hidden_size
+        self.num_heads = config.num_attention_heads
+        self.head_dim = config.head_dim
+        self.num_key_value_heads = config.num_key_value_heads
+        self.num_key_value_groups = self.num_heads // self.num_key_value_heads
+        self.max_position_embeddings = config.max_position_embeddings
+        self.rope_theta = config.rope_theta
+        self.is_casual = True
+
+        assert self.hidden_size % self.num_heads == 0
+
+        self.q_proj = nn.Linear(self.hidden_size, self.num_heads * self.head_dim, bias = config.attention_bias)
+        self.k_proj = nn.Linear(self.hidden_size, self.num_key_value_heads * self.head_dim, bias = config.attention_bias)
+        self.v_proj = nn.Linear(self.hidden_size, self.num_key_value_heads * self.head_dim, bias = config.attention_bias)
+        self.o_proj = nn.Linear(self.num_heads * self.head_dim, self.hidden_size, bias = config.attention_bias)
+
 
 class GemmaDecoderLayer(nn.Module):
 
