35a6b01c5a156d5bc48cc264664947e9f39076e2
IammSwanand <swanandwakadmane94@gmail.com>
2025-12-01 17:44:49 +0530

    added the initiating script for inference

---
+++ initiate_inference.sh
@@ -0,0 +1,18 @@
+MODEL_PATH=""
+PROMPT="this building is"
+IMAGE_FILE_PATH="test_images/pic1.png"
+MAX_TOKEN_TO_GENERATE=100
+TEMPERATURE=0.0
+TOP_P=0.9
+DO_SAMPLE="False"
+ONLY_CPU="False"
+
+python inference.py \
+    --model_path "$MODEL_PATH" \
+    --prompt "$PROMPT" \
+    --image_file_path "$IMAGE_FILE_PATH" \
+    --max_tokens_to_generate $MAX_TOKEN_TO_GENERATE \
+    --temperature $TEMPERATURE \
+    --top_p $TOP_P \
+    --do_sample $DO_SAMPLE \
+    --only_cpu $ONLY_CPU \
